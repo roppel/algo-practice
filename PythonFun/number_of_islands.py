cb86b004c80040ec9bf6fa5d1dfa540e514348ea
Zack Roppel <roppel@gmail.com>
2021-01-31 23:59:51 -0800

    Num of Islands Python cleanup

--- PythonFun/number_of_islands.py
+++ PythonFun/number_of_islands.py
@@ -1,24 +1,22 @@
-class NumberOfIslands():
+def dfs(grid, r, c):
+    if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == 0:
+        return
+    grid[r][c] = 0
+    dfs(grid, r, c - 1)
+    dfs(grid, r, c + 1)
+    dfs(grid, r - 1, c)
+    dfs(grid, r + 1, c)
 
-    def dfs(self, grid, r, c):
-        if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == 0:
-            return
-        grid[r][c] = 0
-        self.dfs(grid, r, c-1)
-        self.dfs(grid, r, c+1)
-        self.dfs(grid, r-1, c)
-        self.dfs(grid, r+1, c)
 
-    def numIslands(self, grid):
-        count = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == 1:
-                    count += 1
-                    self.dfs(grid, row, col)
-        print(count, "count")
+def num_islands(grid):
+    count = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                count += 1
+                dfs(grid, row, col)
+    return count
 
-if __name__ == "__main__":
-    grid = [[1,1,0,0,0], [1,1,0,0,0], [0,0,1,0,0],  [0,0,0,1,1]]
-    NumberOfIslands().numIslands(grid)
 
+matrix = [[1, 1, 0, 0, 0], [1, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 1]]
+print(f"{num_islands(matrix)} count")
